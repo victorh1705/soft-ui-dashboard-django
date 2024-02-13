3cd448c7f48e7014a720b11616bee0f0d75a9411
Victor Henrique <victorh1705@gmail.com>
2024-02-13 21:14:52 +0000

    ci: comment commands used on Dockerfile

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 # exit on error
 set -o errexit
 
-python -m pip install --upgrade pip
-
-pip install -r requirements.txt
+#python -m pip install --upgrade pip
+#
+#pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
 
 chown python:python .env
+chown python:python /home/python/app/.env
 
 gunicorn --config gunicorn-cfg.py core.wsgi
